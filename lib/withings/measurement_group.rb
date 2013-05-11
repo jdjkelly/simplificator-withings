74a4308e05c10214c181b8fc2cc837bc6844704f
Joshua Kelly <jdjkelly@gmail.com>
2013-05-11 17:59:11 -0400

    Moved group_id to grpid, consistent with the actual API response

--- lib/withings/measurement_group.rb
+++ lib/withings/measurement_group.rb
@@ -19,11 +19,11 @@ class Withings::MeasurementGroup
   BLOOD_PRESSURE_MONITOR_TYPES = [TYPE_DIASTOLIC_BLOOD_PRESSURE, TYPE_SYSTOLIC_BLOOD_PRESSURE, TYPE_HEART_PULSE]
   SCALE_TYPES = [TYPE_WEIGHT, TYPE_SIZE, TYPE_FAT_FREE_MASS_WEIGHT, TYPE_FAT_RATIO, TYPE_FAT_MASS_WEIGHT]
 
-  attr_reader :group_id, :attribution, :taken_at, :category
+  attr_reader :grpid, :attribution, :taken_at, :category
   attr_reader :weight, :size, :fat, :ratio, :fat_free, :diastolic_blood_pressure, :systolic_blood_pressure, :heart_pulse
   def initialize(params)
     params = params.stringify_keys
-    @group_id = params['grpid']
+    @grpid = params['grpid']
     @attribution = params['attrib']
     @taken_at = Time.at(params['date'])
     @category = params['category']
@@ -56,7 +56,7 @@ class Withings::MeasurementGroup
   end
 
   def to_s
-    "[ Weight: #{self.weight}, Fat: #{self.fat}, Size: #{self.size}, Ratio: #{self.ratio}, Free: #{self.fat_free}, Blood Pressure: #{self.diastolic_blood_pressure}/#{self.systolic_blood_pressure} @ #{self.heart_pulse}, ID: #{self.group_id} (taken at: #{self.taken_at.strftime("%d.%m.%Y %H:%M:%S")})]"
+    "[ Weight: #{self.weight}, Fat: #{self.fat}, Size: #{self.size}, Ratio: #{self.ratio}, Free: #{self.fat_free}, Blood Pressure: #{self.diastolic_blood_pressure}/#{self.systolic_blood_pressure} @ #{self.heart_pulse}, ID: #{self.grpid} (taken at: #{self.taken_at.strftime("%d.%m.%Y %H:%M:%S")})]"
   end
 
   def inspect
